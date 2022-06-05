30c6f3c83a032f3ba6bb0e6cbc4734d44eda9407
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-06 02:49:40 +0600

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import crypto  from 'crypto';
+import fs from 'fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const path = "./src/hash/files/fileToCalculateHashFor.txt";
+    
+    try {
+        const file = await fs.readFile(path, 'utf-8');
+        const hash = crypto.createHash('sha256').update(file).digest('hex');
+        console.log(hash);
+        return hash;
+    } catch (error) {
+        console.log(error);      
+    }
 };
+
+calculateHash();
